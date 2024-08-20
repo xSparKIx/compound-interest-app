0a0245ec38f0652122ee56ab2471fcc9378da574
SparKI <sparki.projects@yandex.ru>
2024-08-21 00:37:40 +0300

    Исправление стилей и добавление методов расчета процентов.

--- src/app/components/deposit-form/deposit-form.component.ts
+++ src/app/components/deposit-form/deposit-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
 import { InputNumberModule } from 'primeng/inputnumber';
 
 /**
@@ -9,7 +10,12 @@ import { InputNumberModule } from 'primeng/inputnumber';
 @Component({
   selector: 'app-deposit-form',
   standalone: true,
-  imports: [ReactiveFormsModule, InputNumberModule, ButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    InputNumberModule,
+    ButtonModule,
+    CheckboxModule,
+  ],
   templateUrl: './deposit-form.component.html',
   styleUrl: './deposit-form.component.scss',
 })
@@ -25,14 +31,62 @@ export class DepositFormComponent {
       amount: new FormControl<number>(0, [Validators.required, Validators.min(0)]),
       time: new FormControl<number>(0, [Validators.required, Validators.min(0)]),
       percent: new FormControl<number>(0, [Validators.required, Validators.min(0), Validators.max(100)]),
+      reinvest: new FormControl<boolean>(false),
     })
+  }
+
+  /**
+   * Метод рассчета сложного процента без реинвестирования.
+   * @description Рассчитывает сложный процент по формуле: A = P × (1 + r/100 × t), где:
+   * - A - итоговая сумма
+   * - P - начальная сумма
+   * - r - годовая процентная ставка
+   * - t - срок инвестиций в годах
+   * @param p Начальная сумма вложений
+   * @param r Годовая процентная ставка
+   * @param t Cрок инвестиций в годах
+   * @private
+   * @static
+   * @returns Рассчитанный по формуле сложный процент 
+   */
+  private static calcCompoundInterest(p: number, r: number, t: number): number {
+    return p * (1 + r/100 * t);
+  }
+
+  /**
+   * Метод рассчета сложного процента с реинвестированием.
+   * @description Рассчитывает сложный процент с учетом реинвестирования по формуле: A = P × (1 + (r/100)/n)^nt, где:
+   * - A - итоговая сумма
+   * - P - начальная сумма
+   * - r - годовая процентная ставка
+   * - n - количество периодов начисления процентов в год (напр. при ежемесячном начислении процентов количество таких периодов будет 12)
+   * - t - срок инвестиций в годах
+   * @param p Начальная сумма вложений
+   * @param r Годовая процентная ставка
+   * @param n Количество периодов начисления процентов в год
+   * @param t Срок инвестиций в годах
+   * @private
+   * @static
+   * @returns Рассчитанный по формуле сложный процент 
+   */
+  private static calcCIWithReinvest(p: number, r: number, n: number, t: number): number {
+    if (n === 0) {
+      throw new Error('Обязательный параметр "количество периодов" равен 0');
+    }
+
+    return p * (1 + (r/100)/n) ** (n*t);
   }
 
   /**
    * Метод события отправки формы
    */
   public onFormSubmit(): void {
-    const { amount, time, percent } = this.form.value;
-    console.log(amount, time, percent);
+    const { amount, time, percent, reinvest } = this.form.value;
+    
+    if (reinvest) {
+      console.log(DepositFormComponent.calcCIWithReinvest(amount, percent, 12, time));
+    } else {
+      console.log(DepositFormComponent.calcCompoundInterest(amount, percent, time));
+    }
   }
 }
