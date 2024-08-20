4795964a8c375e24128190e1369746e9e8f4830b
SparKI <sparki.projects@yandex.ru>
2024-08-21 01:55:50 +0300

    Добавление логики реинвестирования

--- src/app/components/deposit-form/deposit-form.component.ts
+++ src/app/components/deposit-form/deposit-form.component.ts
@@ -2,7 +2,27 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
 import { InputNumberModule } from 'primeng/inputnumber';
+
+/**
+ * Интерфейс группы формы ввода информации о вкладе
+ */
+interface DepositFormGroup {
+  amount: FormControl<number>;
+  time: FormControl<number>;
+  percent: FormControl<number>;
+  reinvest: FormControl<boolean>;
+  percentFrequency: FormControl<number>;
+}
+
+/**
+ * Элемент выпадающего списка "Период начисления процентов"
+ */
+interface PercentFrequencyItem {
+  readonly label: string;
+  readonly value: number;
+}
 
 /**
  * Компонент формы ввода информации о вкладе
@@ -15,6 +35,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
     InputNumberModule,
     ButtonModule,
     CheckboxModule,
+    DropdownModule,
   ],
   templateUrl: './deposit-form.component.html',
   styleUrl: './deposit-form.component.scss',
@@ -24,15 +45,54 @@ export class DepositFormComponent {
    * Форма ввода информации о вкладе
    * @readonly
    */
-  public readonly form: FormGroup;
+  public readonly form: FormGroup<DepositFormGroup>;
+
+  /**
+   * Период начисления процентов
+   * @readonly
+   */
+  public readonly percentFrequency: PercentFrequencyItem[] = [
+    {
+      label: 'Каждый месяц',
+      value: 12,
+    },
+    {
+      label: 'Каждый квартал',
+      value: 4,
+    },
+    {
+      label: 'Каждое полугодие',
+      value: 2,
+    },
+    {
+      label: 'Каждый год',
+      value: 1,
+    },
+  ];
 
   public constructor() {
     this.form = new FormGroup({
-      amount: new FormControl<number>(0, [Validators.required, Validators.min(0)]),
-      time: new FormControl<number>(0, [Validators.required, Validators.min(0)]),
-      percent: new FormControl<number>(0, [Validators.required, Validators.min(0), Validators.max(100)]),
-      reinvest: new FormControl<boolean>(false),
-    })
+      amount: new FormControl(0, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.min(1)],
+      }),
+      time: new FormControl(0, {
+        nonNullable: true,
+        validators:  [Validators.required, Validators.min(1)],
+      }),
+      percent: new FormControl(0, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.min(1), Validators.max(100)],
+      }),
+      reinvest: new FormControl(false, {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+      percentFrequency: new FormControl(12, {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+    });
   }
 
   /**
@@ -47,7 +107,8 @@ export class DepositFormComponent {
    * @param t Cрок инвестиций в годах
    * @private
    * @static
-   * @returns Рассчитанный по формуле сложный процент 
+   * @returns Рассчитанный по формуле сложный процент
+   * @todo Добавить побитовый вариант и сравнить их
    */
   private static calcCompoundInterest(p: number, r: number, t: number): number {
     return p * (1 + r/100 * t);
@@ -67,7 +128,8 @@ export class DepositFormComponent {
    * @param t Срок инвестиций в годах
    * @private
    * @static
-   * @returns Рассчитанный по формуле сложный процент 
+   * @returns Рассчитанный по формуле сложный процент
+   * @todo Добавить побитовый вариант и сравнить их
    */
   private static calcCIWithReinvest(p: number, r: number, n: number, t: number): number {
     if (n === 0) {
@@ -81,10 +143,26 @@ export class DepositFormComponent {
    * Метод события отправки формы
    */
   public onFormSubmit(): void {
-    const { amount, time, percent, reinvest } = this.form.value;
-    
+    const { amount, time, percent, reinvest, percentFrequency } = this.form.value;
+
+    if (!amount) {
+      throw new Error('Передан пустой параметр "Начальная сумма вложений".')
+    }
+
+    if (!percent) {
+      throw new Error('Передан пустой параметр "Годовая процентная ставка".')
+    }
+
+    if (!time) {
+      throw new Error('Передан пустой параметр "Срок инвестиций в годах".')
+    }
+
     if (reinvest) {
-      console.log(DepositFormComponent.calcCIWithReinvest(amount, percent, 12, time));
+      if (!percentFrequency) {
+        throw new Error('Передан пустой параметр "Количество периодов начисления процентов в год".')
+      }
+
+      console.log(DepositFormComponent.calcCIWithReinvest(amount, percent, percentFrequency, time));
     } else {
       console.log(DepositFormComponent.calcCompoundInterest(amount, percent, time));
     }
